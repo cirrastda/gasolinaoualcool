a34b700ac436c9ddb2d92561227e867f0879cf77
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-04-30 17:51:59 -0300

    Layout

--- gasolinaoualcool/ViewController.swift
+++ gasolinaoualcool/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var txtGasolina: UITextField!
+    @IBOutlet weak var txtAlcool: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func getTextValue(objText: UITextField, tipo: String ) -> Double {
+        var dPreco = 0.0
+        if let sPreco = objText.text {
+            dPreco = Double(sPreco)!
+        } else {
+            lblResult.text = "Informe o Preço "+(tipo == "Gasolina" ? "da" : "do")+" "+tipo
+        }
+        return dPreco
+    }
+    @IBAction func btnCalcular_Click(_ sender: Any) {
+        let dPrecoGasolina = getTextValue(objText: txtGasolina, tipo: "Gasolina")
+        let dPrecoAlcool = getTextValue(objText: txtAlcool, tipo: "Alcool")
+        
+        if dPrecoGasolina>0 && dPrecoAlcool>0 {
+            
+        } else {
+            lblResult.text = "Os valores devem ser maiores que ZERO"
+        }
+    }
+    
 }
 
