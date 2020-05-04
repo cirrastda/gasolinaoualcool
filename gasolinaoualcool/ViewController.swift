24ad0f4078be6b96a897b54c9a25070211c1424b
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-05-04 08:00:39 -0300

    Calculo e validacoes

--- gasolinaoualcool/ViewController.swift
+++ gasolinaoualcool/ViewController.swift
@@ -18,23 +18,52 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func validaCampo(valor: String) -> Bool {
+        var result : Bool = true
+        //var msg_erro
+        let numberCharacters = CharacterSet.decimalDigits.inverted
+        
+        if (valor.isEmpty) {
+            result = false
+        } else if(valor.rangeOfCharacter(from: numberCharacters) == nil) {
+            result = false
+        }
+        return result
+    }
+    
     func getTextValue(objText: UITextField, tipo: String ) -> Double {
         var dPreco = 0.0
         if let sPreco = objText.text {
-            dPreco = Double(sPreco)!
+            if !self.validaCampo(valor: sPreco) {
+                lblResult.text = "Campo "+tipo+" não informado ou inválido"
+            } else {
+                if let tPreco = Double(sPreco) {
+                    dPreco = tPreco
+                } else {
+                    lblResult.text = "Campo "+tipo+" não informado ou inválido"
+                }
+            }
+            
         } else {
             lblResult.text = "Informe o Preço "+(tipo == "Gasolina" ? "da" : "do")+" "+tipo
         }
         return dPreco
     }
+    
+    func calculaMelhorValor(dValorGasolina: Double, dValorAlcool: Double) {
+        let dProp = (dValorAlcool / dValorGasolina)
+        
+        let melhor :String = (dProp >= 0.7 ? "gasolina" : "alcool")
+        lblResult.text = "É melhor usar "+melhor.prefix(1).uppercased()+melhor.dropFirst().lowercased()
+    }
     @IBAction func btnCalcular_Click(_ sender: Any) {
-        let dPrecoGasolina = getTextValue(objText: txtGasolina, tipo: "Gasolina")
-        let dPrecoAlcool = getTextValue(objText: txtAlcool, tipo: "Alcool")
+        let dPrecoGasolina = self.getTextValue(objText: txtGasolina, tipo: "Gasolina")
+        let dPrecoAlcool = self.getTextValue(objText: txtAlcool, tipo: "Alcool")
         
         if dPrecoGasolina>0 && dPrecoAlcool>0 {
-            
+           calculaMelhorValor(dValorGasolina: dPrecoGasolina, dValorAlcool: dPrecoAlcool)
         } else {
-            lblResult.text = "Os valores devem ser maiores que ZERO"
+            // lblResult.text = "Os valores devem ser maiores que ZERO"
         }
     }
     
